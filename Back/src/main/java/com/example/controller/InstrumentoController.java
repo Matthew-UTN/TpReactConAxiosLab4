61828354a9ce6062f46ee3a26174d921192a6789
Matias Ramos <matias.ramos@alumnos.frm.utn.edu.ar>
2020-05-07 21:19:53 -0300

    Update imagen

--- Back/src/main/java/com/example/controller/InstrumentoController.java
+++ Back/src/main/java/com/example/controller/InstrumentoController.java
@@ -2,9 +2,11 @@ package com.example.controller;
 
 import com.example.dto.InstrumentoDTO;
 import com.example.services.InstrumentoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RestController
@@ -15,5 +17,26 @@ public class InstrumentoController extends BaseController<InstrumentoDTO>{
     public InstrumentoController(InstrumentoService instrumentoService) {
         super(instrumentoService);
         this.instrumentoService = instrumentoService;
+    }
+
+    @PostMapping("/uploadImg")
+    public String uploadImg(@RequestParam("imageFile") MultipartFile imageFile) {
+        String value = "";
+        try {
+            instrumentoService.saveImg(imageFile);
+            value = "..\\..\\..\\imagenes\\" + imageFile.getOriginalFilename();
+            return value;
+        } catch (Exception e) {
+            return "Error";
+        }
+    }
+    @DeleteMapping("/deleteImg")
+    public ResponseEntity deleteImg(@PathVariable String path) {
+        try {
+            instrumentoService.deleteImage(path);
+            return ResponseEntity.status(HttpStatus.OK).body("{\"Mensaje\": \"Imagen borrada\"}");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"Mensaje\": \"Error al borrar esta imagen.\"}");
+        }
     }
 }
--- Back/src/main/java/com/example/services/InstrumentoService.java
+++ Back/src/main/java/com/example/services/InstrumentoService.java
@@ -4,6 +4,11 @@ import com.example.dto.InstrumentoDTO;
 import com.example.entidades.Instrumentos;
 import com.example.repository.InstrumentoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class InstrumentoService extends BaseService<Instrumentos, InstrumentoDTO>{
@@ -11,6 +16,25 @@ public class InstrumentoService extends BaseService<Instrumentos, InstrumentoDTO
 
     public InstrumentoService(InstrumentoRepository instrumentoRepository){
         super(instrumentoRepository, InstrumentoDTO.class, Instrumentos.class);
-        this.instrumentoRepository=instrumentoRepository;
+        this.instrumentoRepository = instrumentoRepository;
+    }
+
+    public void saveImg(MultipartFile imageFile) throws Exception {
+        String folder = "/imagenes/";
+        try {
+            byte[] bytes = imageFile.getBytes();
+            Path path = Paths.get(folder + imageFile.getOriginalFilename());
+            Files.write(path, bytes);
+        } catch (Exception e) {
+            throw new Exception();
+        }
+    }
+
+    public void deleteImage(String path) throws Exception {
+        try {
+            Files.deleteIfExists(Paths.get(path));
+        } catch (Exception e) {
+            throw new Exception();
+        }
     }
 }
